5428ce3c9ed79b43a849a71b40d9eca03dc32cb9
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-03-28 19:56:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,155 @@
+function riordanverse() {
+    window.location = "Riordanverse.html";
+}
+
+function pjo() {
+    window.location = "pjo.html";
+}
+
+function LT() {
+    window.location = "The Lightning Thief.pdf";
+}
+
+function SM() {
+    window.location = "The Sea of Monsters.pdf";
+}
+
+function TC() {
+    window.location = "The Titans Curse.pdf";
+}
+
+function BOTL() {
+    window.location = "The Battle of the Labyrinth.pdf";
+}
+
+function LO() {
+    window.location = "The Last Olympian.pdf";
+}
+
+function harryPotter() {
+    window.location = "harryPotter.html";
+}
+
+function PS() {
+    window.location = "Harry Potter and the Philosopher's Stone.docx";
+}
+
+function CS() {
+    window.location = "Harry Potter and the Chamber of Secerets.doc";
+}
+
+function PA() {
+    window.location = "Harry Potter and the Prisoner of Azkaban.doc";
+}
+
+function GOF() {
+    window.location = "Harry Potter and the Goblet of Fire.doc";
+}
+
+function DH() {
+    window.location = "Harry Potter and the Deathly Hallows.docx";
+}
+
+function CH() {
+    window.location = "Harry Potter and the Cursed Child.doc";
+}
+
+function HoO() {
+    window.location = "HoO.html";
+}
+
+function LH() {
+    window.location = "heroes of olympus and the lost hero.pdf";
+}
+
+function SN() {
+    window.location = "heroes of olympus and the son of neptune.pdf";
+}
+
+function MA() {
+    window.location = "Heroes of olympus and the mark of athena.pdf";
+}
+
+function HH() {
+    window.location = "heroes of olympus and the house of hades.pdf";
+}
+
+function BO() {
+    window.location = "Heroes of olympus and the blood of olympus.pdf";
+}
+
+function KC() {
+    window.location = "kaneChronicals.html";
+}
+
+function RP() {
+    window.location = "kane chronicles and the red pyramid.pdf";
+}
+
+function TF() {
+    window.location = "kane chronicles and the throne of fire.pdf";
+}
+
+function SS() {
+    window.location = "kane chronicles and the serpents shadow.pdf";
+}
+
+function ToA() {
+    window.location = "ToA.html";
+}
+
+function HO() {
+    window.location = "Trials of Apollo and the Hidden Oracle.pdf";
+}
+
+function DP() {
+    window.location = "Trials of Apollo and the Dark Prophecy.pdf";
+}
+
+function BM() {
+    window.location = "Trials of Apollo and the Burning Maze.pdf";
+}
+
+function TT() {
+    window.location = "Trials of Apollo the Tyrant’s Tomb.pdf";
+}
+
+function TN() {
+    window.location = "Trials of Apollo and the Tower of Nero.pdf";
+}
+
+function MC() {
+    window.location = "magnusChase.html";
+}
+
+function SoS() {
+    window.location = "Magnus Chase and the Sword of Summer.pdf";
+}
+
+function HT() {
+    window.location = "Magnus Chase and the Hammer of Thor.pdf";
+}
+
+function SD() {
+    window.location = "Magnus Chase and the Ship of the Dead.pdf";
+}
+
+function stClares() {
+    window.location = "stClares.html";
+}
+
+function TSC() {
+    window.location = "St. Clare's Book 1.pdf";
+}
+
+function OST() {
+    window.location = "St. Clare's book 2.pdf";
+}
+
+function STSC() {
+    window.location = "St. Clare's book 3.pdf";
+}
+
+function SFSC() {
+    window.location = "St. Clare's book 4.pdf";
+}
